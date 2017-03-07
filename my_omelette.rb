91f4e2298c268bee36ed4853e0fd34e7df2dffb0
M Sibai <sibaim@gmail.com>
2017-03-07 15:56:59 +0100

    Defining the Ingredients

--- my_omelette.rb
+++ my_omelette.rb
@@ -7,3 +7,13 @@ def print_progress_bar
   3.times { sleep 0.5; print "." }
   puts "\n"
 end
+
+@num_chicken_slides = 2
+@num_cheez_slides = 2
+
+ingredients = [
+  { name: 'chicken', quantity: NUM_POTATOES },
+  { name: 'cheez', quantity: NUM_EGGS },
+  { name: 'oil', quantity: 'some' },
+  { name: 'salt', quantity: 'some' },
+]
